ed62ba3ef7e9b6034b3bd2918e8a7e6c9662c3e9
Ash Monsh <wh7r.com@gmail.com>
2024-04-23 00:04:00 +0300

    add life components

---
+++ app/Filament/Pages/Cigarettes.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Clusters\Life;
+use Filament\Pages\Page;
+
+class Cigarettes extends Page
+{
+    protected static ?string $cluster = Life::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.cigarettes';
+}
